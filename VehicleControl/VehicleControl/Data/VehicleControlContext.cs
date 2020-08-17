8596ef3e7a802a83b20aea073b6b388118de1ad5
williamaiadias <williamaiadias@hotmail.com>
2020-08-16 22:23:49 -0300

    CRUD scaffolding

---
+++ VehicleControl/VehicleControl/Data/VehicleControlContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VehicleControl.Models;
+
+namespace VehicleControl.Data
+{
+    public class VehicleControlContext : DbContext
+    {
+        public VehicleControlContext (DbContextOptions<VehicleControlContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VehicleControl.Models.Marca> Marca { get; set; }
+    }
+}
